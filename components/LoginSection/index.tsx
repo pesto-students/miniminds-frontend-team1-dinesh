f6055da098c24476b7011551d0a96b8a3c81c8d3
devanshdixit <devanshdixit01@gmail.com>
2023-02-27 13:56:02 +0530

    first commit

---
+++ components/LoginSection/index.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+const LoginSection = () => {
+  return (
+    <div className="max-w-[346px] w-full mx-auto pt-28 lg:pt-72 border-gray-700">
+      <div className="flex justify-around">
+        <div className="flex  items-center">
+          <Image
+            className=""
+            src="/assets/miniminds_1.png"
+            height={93}
+            width={93}
+            alt=""
+          />
+          <h3 className="text-[24px] font-[600] leading-[29px] text-[#555555]">
+            MiniMinds
+          </h3>
+        </div>
+      </div>
+      <p className="mx-auto font-[500] text-[12px] leading-[15px] text-center text-[#555555]">
+        Helping educators and students to improve their memory and cognitive
+        skills through fun and engaging gameplay.
+      </p>
+      <form className="mt-9">
+        <div>
+          <label className="flex flex-col text-[16px] leading-[19px] font-[400]">
+            Email
+          </label>
+          <input
+            type="text"
+            placeholder="teachername1.gmail.comp"
+            className="bg-transparent border mt-2 border-black text-[#2E2C29] placeholder:text-[#2E2C29] px-4 py-3 text-[16px] font-[400] leading-[19px] w-full rounded-[5px] py"
+          />
+        </div>
+        <div className="mt-4">
+          <label className="flex flex-col text-[16px] leading-[19px] font-[400]">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="********"
+            className="bg-transparent border mt-2 border-black text-[#2E2C29] placeholder:text-[#2E2C29] px-4 py-3 text-[16px] font-[400] leading-[19px] w-full rounded-[5px] py"
+          />
+        </div>
+        <button className="rounded-[5px] bg-[#19B03D] leading-[19px] text-[16px] mt-5 font-[600] text-white border border-[#19B03D] py-3 text-center flex-1 flex justify-center w-full">
+          Login
+        </button>
+      </form>
+      <div className="relative flex py-5 items-center">
+        <div className="flex-grow border-t border-[#555555]"></div>
+        <span className="flex-shrink mx-4 text-[12px] leading-[15px] font-[400] border-[#555555]">OR</span>
+        <div className="flex-grow border-t border-[#555555]"></div>
+      </div>
+    </div>
+  );
+};
+export default LoginSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import GetStartedSection from "@/components/GetStartedSection";
+import LoginSection from "@/components/LoginSection";
 import Head from "next/head";
 import Image from "next/image";
+import { SyntheticEvent, useState } from "react";
 
 export default function Home() {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
       <Head>
@@ -11,33 +15,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className='w-full h-[1000px] bg-no-repeat bg-center lg:bg-[url("/assets/Rectangle_18.png")]'>
-          <div className="max-w-[346px] w-full mx-auto pt-80 border-gray-700">
-            <Image
-              className="mx-auto text-center"
-              src="/assets/miniminds_1.png"
-              height={190}
-              width={190}
-              alt=""
+        <div className='w-full h-screen overflow-y-scroll lg:h-[1000px] bg-no-repeat bg-center lg:bg-[url("/assets/Rectangle_18.png")]'>
+          {showLogin ? (
+            <LoginSection />
+          ) : (
+            <GetStartedSection
+              onClick={(e: SyntheticEvent) => {
+                e.preventDefault();
+                setShowLogin(true);
+              }}
             />
-            <h3 className="text-[24px] font-[600] leading-[29px] text-center mx-auto text-[#555555]">
-              MiniMinds
-            </h3>
-            <p className="mx-auto font-[500] mt-[10px] text-[12px] leading-[15px] text-center text-[#555555]">
-              Helping educators and students to improve their memory and
-              cognitive skills through fun and engaging gameplay.
-            </p>
-            <button className="rounded-[4px] px-8 py-2 mt-10 flex justify-center text-sm bg-[#19B03D] text-center mx-auto text-white font-semibold ">
-              Get started
-            </button>
-            <p className="text-[10px] mt-32 font-inter font-medium  text-[#555555] mx-auto">
-              By joining, you agree to share contact infornation with people in
-              your organization.
-              <span className="text-[#484FFF] underline decoration-1 	underline-offset-2">
-                Learn More
-              </span>
-            </p>
-          </div>
+          )}
         </div>
       </main>
     </>
